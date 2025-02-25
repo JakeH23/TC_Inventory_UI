84b4357b6795ad9d4f5072dc7da050920e6f8b0b
JakeH23 <42804816+JakeH23@users.noreply.github.com>
2025-02-25 00:24:32 +0000

    fix enviroment name

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    baseUrl: "https://tonys-garage-597e0a2bdc99.herokuapp.com/"
+    baseUrl: "https://tonys-garage-597e0a2bdc99.herokuapp.com"
 };
